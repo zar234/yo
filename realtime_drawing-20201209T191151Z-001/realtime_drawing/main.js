3b8c88dd917e2bae8c066c2ef13e3c963d88c3dd
zar234 <101414581+zar234@users.noreply.github.com>
2022-03-21 16:19:28 +0530

    Add files via upload

---
+++ realtime_drawing-20201209T191151Z-001/realtime_drawing/main.js
@@ -0,0 +1,48 @@
+noseX = 0;
+noseY = 0;
+difference= 0;
+rightWristX= 0;
+leftWristX= 0;
+function setup ()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+console.log('PoseNet is Initialized');
+}
+
+function draw()
+{
+    background ('#800080');
+
+    document.getElementById("square_size").innerHTML = "Width and Height of a square will be" + difference +"px";
+
+    fill('#FF0000');
+    stroke('#000000');
+    square(noseX, noseY, difference);
+}
+
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX =" + noseX + "noseY" + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor (leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+    }
+}
